669251441d8a0f6793fe3ab6bbe15a749b6aa67f
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-04-06 16:17:25 +0200

    if true, show description

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 function Home() {
+  let hiedeDescription = true;
   return (
     <main>
       <Header title="Title" textSub="text sub" />
-      <Description />
+      {hiedeDescription && <Description />}  {/* if true, show description*/}
       <Footer />
     </main>
   );
