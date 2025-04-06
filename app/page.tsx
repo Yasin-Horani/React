aa1e3c99820ed3fd6afd53ea23bc434265c29e71
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-04-06 15:22:33 +0200

    react fragment <></>

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,32 @@ function Header() {
 function Footer() {
   return <h1>Footer</h1>;
 }
+function List() {
+  return (
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+  );
+}
+
+function Description() {
+  // react fragment <></>
+  return (
+    <>
+      <h2>List of items</h2>
+      <List />
+    </>
+  );
+}
 
 function Home() {
   return (
     <main>
       <Header />
       <h1>Hallo wereld</h1>
+      <Description />
       <Footer />
     </main>
   );
