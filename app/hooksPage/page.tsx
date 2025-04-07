ce6a7000e3651064d614ed9caa080c727b40730d
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-04-07 12:39:50 +0200

    inputRef

--- app/hooksPage/page.tsx
+++ app/hooksPage/page.tsx
@@ -1,55 +1,22 @@
 "use client";
-import { log } from "console";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+
+import { useRef } from "react";
 
 function HooksPage() {
-  const [counter, setCounter] = useState<number>(0);
-  // This effect runs when the component mounts and when the `success` or `counter` state changes
-  const [success, setSuccess] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  const incrementCounter = () => {
-    if (counter < 100) {
-      setCounter(counter + 1);
-      if (counter === 10) {
-        setSuccess(true);
-      }
+  const handleClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
     }
+    alert(inputRef.current?.value);
+    inputRef.current!.value = "";
   };
-
-  const decrementCounter = () => {
-    if (counter > 0) {
-      setCounter(counter - 1);
-    }
-  };
-// This effect runs when the component mounts and when the `success` state changes
-  useEffect(() => {
-    console.log("Welcome to the Hooks Page!");
-    alert("Welcome to the Hooks Page!");
-  }, [success]);
- 
-  
-  // This effect runs when the component mounts and when the `counter` state changes
-  useEffect(() => {
-    console.log("counter is 10!");	
-    alert("coutner is 10!");
-  }, [counter === 10]);
-
 
   return (
     <div>
-      <div>Counter: {counter}</div>
-      <button onClick={incrementCounter} disabled={counter >= 100}>
-        Click to Increment
-      </button>
-      <br />
-      <button onClick={decrementCounter} disabled={counter <= 0}>
-        Click to Decrement
-      </button>
-      <br />
-      <Link href="/">
-        <strong>go to home</strong>
-      </Link>
+      <input ref={inputRef} />
+      <button onClick={handleClick}>Focus Input</button>
     </div>
   );
 }
