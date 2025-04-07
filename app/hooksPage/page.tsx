fc5470728acfdce6550d626f988435ec4e2fda8b
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-04-07 12:12:21 +0200

    This effect runs when the component mounts and when the `success` or `counter` state changes

--- app/hooksPage/page.tsx
+++ app/hooksPage/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function HooksPage() {
   const [counter, setCounter] = useState<number>(0);
+  // This effect runs when the component mounts and when the `success` or `counter` state changes
+  const [success, setSuccess] = useState<boolean>(false);
 
   const incrementCounter = () => {
     if (counter < 100) {
       setCounter(counter + 1);
+      if (counter === 10) {
+        setSuccess(true);
+      }
     }
   };
 
@@ -16,6 +21,11 @@ function HooksPage() {
       setCounter(counter - 1);
     }
   };
+// This effect runs when the component mounts and when the `success` or `counter` state changes
+  useEffect(() => {
+    alert("Welcome to the Hooks Page!");
+  }, [success, counter]);
+
 
   return (
     <div>
